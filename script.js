0c94a28eddc87ef7c93aa3d95f89b8aa866afa6d
unknown <tadelalrmony@gmail.com>
2022-10-11 18:13:26 +0200

    new fix

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 let button = document.querySelector(".icon-upload"),
     inputsValue = document.querySelector("[name='task']");
     
- 
-let taskCounter ;
+     
+let taskCounter  ;
 localStorage.getItem('tasksCounter') ? taskCounter = localStorage.getItem('tasksCounter') : taskCounter = 0 ;
 
-let taskCounterFun = _=> localStorage.setItem('tasksCounter', taskCounter);
+let taskCounterFun = _=> localStorage.setItem('tasksCounter', taskCounter++);
+let storeData = (value) => {localStorage.setItem(`task ${taskCounter}`, value);  taskCounterFun()} ,
+    getData = (value) => { let taskName = `task ${value}`; return JSON.parse(localStorage.getItem(taskName));};
 
-let storeData = (value) => {localStorage.setItem(`task ${taskCounter}`, value); taskCounter++; taskCounterFun()} ,
-    getData = (value) => localStorage.getItem(value);
+
 
 button.addEventListener('click', function (event){
-
     event.preventDefault();
-
     let taskData = {task: inputsValue.value, done: false, star: true};
+     storeData(JSON.stringify(taskData));
 
-     storeData(JSON.stringify(taskData))  ;
-
-    let res = JSON.parse(getData(taskCounter)) ;
-
-    console.log(`${res.task} & ${taskCounter}`);
-    
+    let res = getData(taskCounter) ;
+    console.log(`${res.task} & ${taskCounter}`);   
 }); 
