ac8ced096eb3fa88e2a921476bbe551e823d8db3
unknown <tadelalrmony@gmail.com>
2022-10-11 20:15:05 +0200

    new features

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 let button = document.querySelector(".icon-upload"),
-    inputsValue = document.querySelector("[name='task']");
+    inputsValue = document.querySelector("[name='task']"),
+    allTasksElement = document.querySelector("#allTasks"),
+    starElement = document.querySelector(".icon-star");
     
-     
-let taskCounter  ;
+    
+   
+//let taskCounter = 0 ;
 localStorage.getItem('tasksCounter') ? taskCounter = localStorage.getItem('tasksCounter') : taskCounter = 0 ;
 
-let taskCounterFun = _=> localStorage.setItem('tasksCounter', taskCounter++);
-let storeData = (value) => {localStorage.setItem(`task ${taskCounter}`, value);  taskCounterFun()} ,
+let taskCounterFun = _=> localStorage.setItem('tasksCounter', ++taskCounter);
+let storeData = (value) => { taskCounterFun(); localStorage.setItem(`task ${taskCounter}`, value); } ,
     getData = (value) => { let taskName = `task ${value}`; return JSON.parse(localStorage.getItem(taskName));};
 
-
+    
+for(let i=taskCounter; i <= taskCounter ; --i){
+    if(i == 0 ) {break;}
+   let allTasks = getData(i);
+    allTasksElement.innerHTML +=`
+    <div class="task" id="${i}">
+          <span class="icon-star icon ${allTasks.star == true ? 'stared' : '' }"></span>
+          <p>${allTasks.task}</p>
+          <div>
+              <span class="icon-trash icon" data-id=${i}></span>
+              <span class="icon-angry icon"></span>
+          </div>
+      </div>`;
+      
+}
 
 button.addEventListener('click', function (event){
     event.preventDefault();
-    let taskData = {task: inputsValue.value, done: false, star: true};
-     storeData(JSON.stringify(taskData));
-
-    let res = getData(taskCounter) ;
-    console.log(`${res.task} & ${taskCounter}`);   
+    let taskData = {task: inputsValue.value, done: false, star: false};
+    storeData(JSON.stringify(taskData));
+    location.reload();
+      
 }); 
+
